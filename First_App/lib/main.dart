86b95ac2dffa293d8a72baf3577e80533e9dd3e9
jinhaEom <umjin98@naver.com>
2022-03-21 14:53:53 +0900

    text padding값

--- First_App/lib/main.dart
+++ First_App/lib/main.dart
@@ -8,34 +8,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
+      title: 'Charactor App',
       theme: ThemeData(
           primarySwatch: Colors.blue
       ),
-      home: MyHomePage(),
+      home: MyCard(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyCard extends StatelessWidget {
+  const MyCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First App'),
+        title: Text('BBANTO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0 //높이
       ),
-    body: Center(
-      child: Column(
-        children: [
-          Text('Hello'),
-          Text('Hello'),
-          Text('Hello')
+      body:  Center(
+        child: Column(
+           // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello'),
+              Text('Hello'),
+              Text('Hello')
+            ],
+          ),
+      ),
 
-        ],
-      ),
-    ),
     );
   }
 }
+
 
